440f8eb91c63c27aee61bea9d9343b88373cef09
zany <muguliebe@gmail.com>
2021-03-31 01:09:44 +0900

    script

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,12 @@
 import Link from 'next/link'
+import GoogleAnalyticsHOC from '../lib/gtag'
 
 export default function Header() {
   return (
     <header>
       <title>egstep</title>
       <link rel="icon" href="/favicon.ico" />
+      <GoogleAnalyticsHOC/>
     </header>
   )
 }

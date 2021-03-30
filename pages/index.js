df47415d61a70d3226a5a3c2aaea3db79ffd07b6
zany <muguliebe@gmail.com>
2021-03-30 22:33:02 +0900

    init

--- pages/index.js
+++ pages/index.js
@@ -5,48 +5,39 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>egstep</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">egstep</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          playground by
+          <code className={styles.code}>@zany</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://blog.egstep.com" className={styles.card}>
+            <h3>blog &rarr;</h3>
+            <p>programming diary</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="https://apij.egstep.com" className={styles.card}>
+            <h3>example Spring &rarr;</h3>
+            <p>code with spring boot</p>
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://api.egstep.com"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h3>example Node &rarr;</h3>
+            <p>code with Express.js</p>
           </a>
+
         </div>
       </main>
 
@@ -57,7 +48,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src="/egstep_large.png" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
